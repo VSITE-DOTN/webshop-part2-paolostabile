6c512df9c5a1f767a62b1a278ac7c534aaeed0de
Paolo Stabile <pastabil@vsite.hr>
2024-05-06 22:17:08 +0200

    Added Pagination for product search.

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -17,6 +17,11 @@ public class ProductService : IProductService
 
     public string Message { get; set; } = "Loading products...";
 
+    public int CurrentPage { get; set; } = 1;
+    public int PageCount { get; set; } = 0;
+    public string LastSearchText { get; set; } = string.Empty;
+
+
     public event Action ProductsChanged;
 
     public async Task<ServiceResponse<Product>> GetProduct(int productId)
@@ -33,6 +38,12 @@ public class ProductService : IProductService
         if (result != null && result.Data != null)
             Products = result.Data;
 
+        CurrentPage = 1;
+        PageCount = 0;
+
+        if (Products.Count == 0)
+            Message = "No products found";
+
         ProductsChanged.Invoke();
     }
 
@@ -43,12 +54,17 @@ public class ProductService : IProductService
         return result.Data;
     }
 
-    public async Task SearchProducts(string searchText)
+    public async Task SearchProducts(string searchText, int page)
     {
+        LastSearchText = searchText;
         var result = await _http
-             .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+             .GetFromJsonAsync<ServiceResponse<ProductSearchResult>>($"api/product/search/{searchText}/{page}");
         if (result != null && result.Data != null)
-            Products = result.Data;
+        {
+            Products = result.Data.Products;
+            CurrentPage = result.Data.CurrentPage;
+            PageCount = result.Data.Pages;
+        }
         if (Products.Count == 0) Message = "No products found.";
         ProductsChanged?.Invoke();
     }
--- Server/Services/ProductService/IProductService.cs
+++ Server/Services/ProductService/IProductService.cs
@@ -8,7 +8,7 @@ public interface IProductService
     Task<ServiceResponse<List<Product>>> GetProductsAsync();
     Task<ServiceResponse<Product>> GetProductAsync(int productId);
     Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
-    Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
+    Task<ServiceResponse<ProductSearchResult>> SearchProducts(string searchText, int page);
     Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText);
     Task<ServiceResponse<List<Product>>> GetFeaturedProducts();
 }

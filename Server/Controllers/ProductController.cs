df61cda8e56e891db4454252ff128743ba8edd6a
Paolo Stabile <pastabil@vsite.hr>
2024-05-06 22:07:27 +0200

    Added FeaturedProducts feature.

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -50,4 +50,11 @@ public class ProductController : ControllerBase
         var result = await _productService.GetProductSearchSuggestions(searchText);
         return Ok(result);
     }
+
+    [HttpGet("featured")]
+    public async Task<ActionResult<ServiceResponse<List<Product>>>> GetFeaturedProducts()
+    {
+        var result = await _productService.GetFeaturedProducts();
+        return Ok(result);
+    }
 }

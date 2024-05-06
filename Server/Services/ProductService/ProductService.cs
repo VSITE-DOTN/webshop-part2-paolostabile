528ee1e8923d9d676b328c9b3cc4edfb17743c0b
Paolo Stabile <pastabil@vsite.hr>
2024-05-06 21:52:43 +0200

    Added ProductVariant & ProductType implementation.

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -17,7 +17,10 @@ public class ProductService : IProductService
     public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
     {
         var response = new ServiceResponse<Product>();
-        var product = await _context.Products.FindAsync(productId);
+        var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
         if (product == null)
         {
             response.Success = false;
@@ -35,7 +38,7 @@ public class ProductService : IProductService
     {
         var response = new ServiceResponse<List<Product>>
         {
-            Data = await _context.Products.ToListAsync()
+            Data = await _context.Products.Include(p => p.Variants).ToListAsync()
         };
 
         return response;
@@ -47,6 +50,7 @@ public class ProductService : IProductService
         {
             Data = await _context.Products
                 .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .Include(p => p.Variants)
                 .ToListAsync()
         };
 

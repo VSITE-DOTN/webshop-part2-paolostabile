c703f41fa47fe54b36f982bb417bc8f4774c4f42
Paolo Stabile <pastabil@vsite.hr>
2024-05-06 22:19:04 +0200

    Added image url fix.

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -72,7 +72,7 @@ public class DataContext : DbContext
                     Id = 3,
                     Title = "Nineteen Eighty-Four",
                     Description = "Nineteen Eighty-Four (also stylised as 1984) is a dystopian social science fiction novel and cautionary tale written by English writer George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime. Thematically, it centres on the consequences of totalitarianism, mass surveillance and repressive regimentation of people and behaviours within society.[2][3] Orwell, a democratic socialist, modelled the totalitarian government in the novel after Stalinist Russia and Nazi Germany.[2][3][4] More broadly, the novel examines the role of truth and facts within politics and the ways in which they are manipulated.",
-                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/5/51/1984_first_edition_cover.jpg",
                     CategoryId = 1
                 },
                 new Product

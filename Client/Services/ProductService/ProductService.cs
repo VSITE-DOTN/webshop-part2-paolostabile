5f53d4fc08d9ad66286d6f36a169b54522d82567
Paolo Stabile <pastabil@vsite.hr>
2024-05-06 21:57:52 +0200

    Added ProductSearch feature.

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -15,6 +15,8 @@ public class ProductService : IProductService
 
     public List<Product> Products { get; set; } = new List<Product>();
 
+    public string Message { get; set; } = "Loading products...";
+
     public event Action ProductsChanged;
 
     public async Task<ServiceResponse<Product>> GetProduct(int productId)
@@ -32,5 +34,22 @@ public class ProductService : IProductService
             Products = result.Data;
 
         ProductsChanged.Invoke();
+    }
+
+    public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+    {
+        var result = await _http
+            .GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+        return result.Data;
+    }
+
+    public async Task SearchProducts(string searchText)
+    {
+        var result = await _http
+             .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+        if (result != null && result.Data != null)
+            Products = result.Data;
+        if (Products.Count == 0) Message = "No products found.";
+        ProductsChanged?.Invoke();
     }
 }
--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -36,4 +36,18 @@ public class ProductController : ControllerBase
         var result = await _productService.GetProductsByCategory(categoryUrl);
         return Ok(result);
     }
+
+    [HttpGet("search/{searchText}")]
+    public async Task<ActionResult<ServiceResponse<List<Product>>>> SearchProducts(string searchText)
+    {
+        var result = await _productService.SearchProducts(searchText);
+        return Ok(result);
+    }
+
+    [HttpGet("searchsuggestions/{searchText}")]
+    public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductSearchSuggestions(string searchText)
+    {
+        var result = await _productService.GetProductSearchSuggestions(searchText);
+        return Ok(result);
+    }
 }
